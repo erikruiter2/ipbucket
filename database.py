e058af11feb75a2fcc073b77c79c1b3021340830
Erik Ruiter <erikr@mlt0078.home>
2017-04-14 00:32:45 +0200

    updated formatting to PEP8, added delete function for IPDomain and IPv4Address

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from ipaddr_func import ip2long, long2ip
+from ipaddr_func import long2ip
 from sqlalchemy.exc import IntegrityError
 
 engine = create_engine('sqlite:////tmp/ipbucket.db', convert_unicode=True)
@@ -12,8 +12,7 @@ Base = declarative_base()
 Base.query = db_session.query_property()
 
 
-
-def init_db(): 
+def init_db():
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
@@ -22,44 +21,62 @@ def init_db():
 
 
 def db_add_entry(tablename, **kwargs):
-  """ Adds an entry to a table, returns an errormessage on failure, on success it returns the id of the created object"""
-  try:
-    n = tablename(**kwargs)
-    db_session.add(n)
-    db_session.commit()
-  except ValueError as e:
-    return "Error while adding entry: " + str(e)
-  except TypeError as e:
-    return "Error while adding entry: " + str(e)
-  except IntegrityError as e:
-    db_session.rollback()
-    return "Error while adding entry: " + str(e.orig.message) + str(e.params)   
-  return n.id
+    """ Adds an entry to a table, returns an errormessage on failure, on success it returns the id of the created object"""
+    try:
+        n = tablename(**kwargs)
+        db_session.add(n)
+        db_session.commit()
+    except ValueError as e:
+        return "Error while adding entry: " + str(e)
+    except TypeError as e:
+        return "Error while adding entry: " + str(e)
+    except IntegrityError as e:
+        db_session.rollback()
+        return "Error while adding entry: " + str(e.orig.message) + str(e.params)
+    return n.id
 
- 
+
+def db_delete_entry(tablename, **kwargs):
+    """ Deletes an entry from a table, returns an errormessage on failure, on success it returns True"""
+
+    try:
+        entry = tablename.query.filter_by(**kwargs).first()
+        if entry:
+            db_session.delete(entry)
+            db_session.commit()
+        else:
+            return "Entry not found"
+    except ValueError as e:
+        return "Error while deleting entry: " + str(e)
+    except TypeError as e:
+        return "Error while deleting entry: " + str(e) 
+    except IntegrityError as e:
+        db_session.rollback()
+        return "Error while deleting entry: " + str(e.orig.message) + str(e.params)
+    return True
+
+
 def db_query_all(tablename):
-  print tablename.__tablename__
-  entries = list()
-  entrylist = dict()
-
-  for entry in tablename.query.all():
-    for value in vars(entry):
-      if value != '_sa_instance_state':
-        entrylist[value] = vars(entry)[value]
-      if value == 'ip' and tablename.__tablename__ == 'ipv4network':
-		    entrylist[value] = long2ip(vars(entry)[value])  + "/" + str(vars(entry)['mask']) 	
-
-    entries.append(entrylist)
+    print tablename.__tablename__
+    entries = list()
     entrylist = dict()
-  return entries
 
+    for entry in tablename.query.all():
+        for value in vars(entry):
+            if value != '_sa_instance_state':
+                entrylist[value] = vars(entry)[value]
+            if value == 'ip' and tablename.__tablename__ == 'ipv4network':
+                entrylist[value] = long2ip(vars(entry)[value]) + "/" + str(vars(entry)['mask'])
+
+        entries.append(entrylist)
+        entrylist = dict()
+    return entries
 
 
 def db_query_by_id(tablename, _id):
-  entrylist = dict()
-  entry = tablename.query.filter_by(id=_id).first()
-  for value in vars(entry):
-    if value != '_sa_instance_state':
-      entrylist[value] = vars(entry)[value]
-  return entrylist
-
+    entrylist = dict()
+    entry = tablename.query.filter_by(id=_id).first()
+    for value in vars(entry):
+        if value != '_sa_instance_state':
+            entrylist[value] = vars(entry)[value]
+    return entrylist
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 """
 Overview of Templates:
-base.html: 
+base.html:
  - Global html structure containing <html> and <body> tag
 index.html
  - block maincontent: initial static welcome page
@@ -11,18 +11,16 @@ base_elements.html
  - macro datatable(id, columns): Creates an empty jquery table with specified columns
 """
 
-import sys
-import settings
-import json
-import ipaddr_func
-from database import init_db, db_session, db_query_by_id, db_add_entry, db_query_all
-from models import IPv4Network, IPDomain, DomainIPv4, IPv4Address
-from sqlalchemy import create_engine, MetaData, update
-from flask import Flask, request, render_template, Response, Blueprint, jsonify
-app = Flask(__name__)
-from ipaddr_func import ip2long, long2ip, is_valid_ipv4_address, is_valid_ipv6_address
 import logging
+import json
+from database import init_db, db_session, db_query_by_id, db_add_entry, db_delete_entry, db_query_all
+from models import IPv4Network, IPDomain, DomainIPv4, IPv4Address
+from sqlalchemy import update
+from flask import Flask, request, render_template, Blueprint, jsonify
+from ipaddr_func import ip2long, long2ip, is_valid_ipv4_address, is_valid_ipv6_address
 from logging.handlers import RotatingFileHandler
+
+app = Flask(__name__)
 
 init_db()
 
@@ -31,242 +29,277 @@ wsgi_app = Blueprint('wsgi_app', __name__)
 handler = RotatingFileHandler('ipbucket.log', maxBytes=10000, backupCount=1)
 handler.setLevel(logging.INFO)
 app.logger.addHandler(handler)
-def response(result = "", notification = "", request_info = None):
-  return jsonify( { 'result' : result , 'notification' : notification, 'request_info': dict(request.view_args) })
 
 
+def response(result="", notification="", request_info=None):
+    return jsonify({'result': result, 'notification': notification, 'request_info': dict(request.view_args)})
 
 
 """ WEB SERVICE routing """
 
+
 @wsgi_app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
+
 
 @wsgi_app.route('/ip_domain', methods=['GET'])
 def ip_domain():
-  return render_template('ip_domain_overview.html')
+    return render_template('ip_domain_overview.html')
+
 
 @wsgi_app.route('/ip_domain/add/', methods=['GET'])
 def ip_domain_add():
-  return render_template('ip_domain_add_form.html')
+    return render_template('ip_domain_add_form.html')
+
 
 @wsgi_app.route('/ip_network/<int:domain_id>', methods=['GET'])
 def ip_network_overview(domain_id):
-  return render_template('ip_network_overview.html')
+    return render_template('ip_network_overview.html')
+
 
 @wsgi_app.route('/ip_network/<int:ip_domain_id>/<int:network_id>', methods=['GET'])
-def ip_network(ip_domain_id,network_id):
-  return render_template('ip_network.html', data = db_query_IPv4Network(ip_domain_id=ip_domain_id, network_id=network_id))
+def ip_network(ip_domain_id, network_id):
+    return render_template('ip_network.html', data=db_query_IPv4Network(ip_domain_id=ip_domain_id, network_id=network_id))
+
 
 @wsgi_app.route('/ip_network/add/', methods=['GET'])
 def ip_network_add():
-  return render_template('ip_network_add_form.html')
-
+    return render_template('ip_network_add_form.html')
 
 
 """ REST API routing """
 
 
 """ GET requests """
+
+
 @wsgi_app.route('/api/ip_domain/all', methods=['GET'])
 def api_ip_domains_get():
-  return response(result = db_query_all(IPDomain))
+    return response(result=db_query_all(IPDomain))
+
 
 @wsgi_app.route('/api/ip_domain/overview', methods=['GET'])
 def api_ip_domains_overview_get():
-  return response(result = db_query_IPDomainOverview())
+    return response(result=db_query_IPDomainOverview())
+
 
 @wsgi_app.route('/api/ip_domain/<int:id>', methods=['GET'])
 def api_ip_domain_get(id):
-  return response(result = db_query_by_id(IPDomain, id))
+    return response(result=db_query_by_id(IPDomain, id))
+
 
 @wsgi_app.route('/api/ip_network/<int:ip_domain_id>', methods=['GET'])
 def api_ip_networks_get(ip_domain_id):
-  return response(result = db_query_all_IPv4Network(ip_domain_id))
+    return response(result=db_query_all_IPv4Network(ip_domain_id))
+
 
 @wsgi_app.route('/api/ip_network/<int:ip_domain_id>/<int:network_id>', methods=['GET'])
 def api_ip_network_get(ip_domain_id, network_id):
-  return response(result = db_query_IPv4Network(ip_domain_id=ip_domain_id, network_id=network_id))
+    return response(result=db_query_IPv4Network(ip_domain_id=ip_domain_id, network_id=network_id))
+
 
 @wsgi_app.route('/api/ip_address/<int:ip_domain_id>/<int:ip>/<int:count>', methods=['GET'])
 def api_ip_address_get(ip_domain_id, ip, count):
-  print request.view_args
-  return response(result = db_query_IPv4Address(ip_domain_id=ip_domain_id, ip=ip, count=count))
+    print request.view_args
+    return response(result=db_query_IPv4Address(ip_domain_id=ip_domain_id, ip=ip, count=count))
 
 
 """ POST requests """
+
+
 @wsgi_app.route('/api/ip_domain/', methods=['POST'])
 def api_ip_domain_post():
-  data = json.loads(request.data)
-  result =  db_add_entry(IPDomain,name=data['name'], comment=data['comment'])
-  if type(result) is str:
-    return response('error',result)
-  else:
-    return response('success', 'IP Domain ' + data['name'] + ' succesfully created.')
+    data = json.loads(request.data)
+    result = db_add_entry(IPDomain, name=data['name'], comment=data['comment'])
+    if type(result) is str:
+        return response('error', result)
+    else:
+        return response('success', 'IP Domain ' + data['name'] + ' succesfully created.')
+
 
 @wsgi_app.route('/api/ip_network/', methods=['POST'])
 def api_ip_network_post():
-  data = json.loads(request.data)
-  result = db_add_IPv4Network(ip_domain_id=data['ip_domain'], name=data['name'], address=data['address'], comment=data['comment'])
-  if type(result) is str:
-    return response('error',result)
-  else:
-    return response('success', 'Network ' + data['name'] + ' with address ' + data['address'] + ' succesfully created.')
+    data = json.loads(request.data)
+    result = db_add_IPv4Network(ip_domain_id=data['ip_domain'], name=data['name'], address=data['address'], comment=data['comment'])
+    if type(result) is str:
+        return response('error', result)
+    else:
+        return response('success', 'Network ' + data['name'] + ' with address ' + data['address'] + ' succesfully created.')
 
 
 @wsgi_app.route('/api/ip_address/', methods=['POST'])
 def api_ip_address_post():
-  data = json.loads(request.data)
-  result = db_add_entry(IPv4Address,ip=data['ip'], ip_domain_id=data['ip_domain_id'], fqdn=data['fqdn'], description=data['description'], reserved=data['reserved'])
-  if type(result) is str:
-    return response('error',result)
-  else:
-    return response('success', 'IP Address entry ' + data['fqdn'] + ' succesfully created.')
+    data = json.loads(request.data)
+    result = db_add_entry(IPv4Address, ip=data['ip'], ip_domain_id=data['ip_domain_id'], fqdn=data['fqdn'], description=data['description'], reserved=data['reserved'])
+    if type(result) is str:
+        return response('error', result)
+    else:
+        return response('success', 'IP Address entry ' + data['fqdn'] + ' succesfully created.')
 
 
 """ PUT requests """
+
+
 @wsgi_app.route('/api/ip_address/<int:ip_domain_id>/<int:ip>', methods=['PUT'])
-def api_ip_address_put(ip_domain_id,ip):
-  data = json.loads(request.data)
-  result = db_change_IPv4Address(ip=ip, ip_domain_id=ip_domain_id, data=data)
-  if type(result) is str:
-    return response('error',result)
-  else:
-    return response('success', 'IP Address entry ' + long2ip(ip) + ' succesfully updated.')
+def api_ip_address_put(ip_domain_id, ip):
+    data = json.loads(request.data)
+    result = db_change_IPv4Address(ip=ip, ip_domain_id=ip_domain_id, data=data)
+    if type(result) is str:
+        return response('error', result)
+    else:
+        return response('success', 'IP Address entry ' + long2ip(ip) + ' succesfully updated.')
+
+
+""" DELETE requests """
+
+
+@wsgi_app.route('/api/ip_domain/<int:ip_domain_id>', methods=['DELETE'])
+def api_ip_domain_delete(ip_domain_id):
+    result = db_delete_entry(IPDomain, id=ip_domain_id)
+    if type(result) is str:
+        return response('error', result)
+    else:
+        return response('success', 'IP Domain entry ' + str(ip_domain_id) + ' succesfully deleted.')
+
+
+@wsgi_app.route('/api/ip_address/<int:ip_domain_id>/<int:ip>', methods=['DELETE'])
+def api_ip_address_delete(ip_domain_id, ip):
+    result = db_delete_entry(IPv4Address, ip=ip, ip_domain_id=ip_domain_id)
+    if type(result) is str:
+        return response('error', result)
+    else:
+        return response('success', 'IP Address entry ' + long2ip(ip) + ' succesfully deleted.')
+
 
 
 """ Helper functions """
 
 
 def db_add_IPv4Network(ip_domain_id, name, address, comment):
-  result=0
-  ip , mask = address.split("/")
-  print ip, mask
-  if (is_valid_ipv4_address(ip) == False):
-    return str(ip + " is not a valid IPv4 address.")
-  
-  result = db_add_entry(IPv4Network,ip_domain_id=ip_domain_id, name=name, ip=ip2long(ip), mask=int(mask), comment=comment)
-  
-  if type(result) is int:
-    result = db_add_entry(DomainIPv4,ip_domain_id=ip_domain_id, ip4_network_id=result)
-  
-  return result
+    result = 0
+    ip, mask = address.split("/")
+    print ip, mask
+    if (is_valid_ipv4_address(ip) is False):
+        return str(ip + " is not a valid IPv4 address.")
+
+    result = db_add_entry(IPv4Network, ip_domain_id=ip_domain_id, name=name, ip=ip2long(ip), mask=int(mask), comment=comment)
+
+    if type(result) is int:
+        result = db_add_entry(DomainIPv4, ip_domain_id=ip_domain_id, ip4_network_id=result)
+
+    return result
+
 
 def db_query_IPDomainOverview():
-  ip_domains = db_query_all(IPDomain)
-  for ip_domain in ip_domains:
-    ipv4network_count =  db_session.query(DomainIPv4.id).filter(DomainIPv4.ip_domain_id==ip_domain['id']).count() 
-    ip_domain['ipv4network_count'] = ipv4network_count
-    ip_domain['ipv6network_count'] = 0
-  return ip_domains
+    ip_domains = db_query_all(IPDomain)
+    for ip_domain in ip_domains:
+        ipv4network_count = db_session.query(DomainIPv4.id).filter(DomainIPv4.ip_domain_id == ip_domain['id']).count()
+        ip_domain['ipv4network_count'] = ipv4network_count
+        ip_domain['ipv6network_count'] = 0
+    return ip_domains
+
 
 def db_query_all_IPv4Network(ip_domain_id):
-  entries = list()
-  entrylist = dict()
-  result = IPv4Network.query.order_by(IPv4Network.ip.asc(), IPv4Network.mask.asc() )
-  index = 0
-  for entry in result:
-    entrylist['index'] = index
-    index += 1
-    for value in vars(entry):
-      if value != '_sa_instance_state':
-        entrylist[value] = vars(entry)[value]
-        entrylist['parent'] = 0
-
-    entries.append(entrylist)
+    entries = list()
     entrylist = dict()
-  entries = add_parents(entries)
-  entries = add_utilisation(entries)
-  for i in range (0, len(entries)):
-    entries[i]['ip'] = long2ip(entries[i]['ip'])  + "/" + str(entries[i]['mask'])
-  
-  return entries
+    result = IPv4Network.query.order_by(IPv4Network.ip.asc(), IPv4Network.mask.asc())
+    index = 0
+    for entry in result:
+        entrylist['index'] = index
+        index += 1
+        for value in vars(entry):
+            if value != '_sa_instance_state':
+                entrylist[value] = vars(entry)[value]
+                entrylist['parent'] = 0
+
+        entries.append(entrylist)
+        entrylist = dict()
+    entries = add_parents(entries)
+    entries = add_utilisation(entries)
+    for i in range(0, len(entries)):
+        entries[i]['ip'] = long2ip(entries[i]['ip']) + "/" + str(entries[i]['mask'])
+
+    return entries
+
 
 def add_parents(entries):
-  for p in entries:
-    for c in entries:
-      if c['index'] is not p['index']:
-        if (IPv4Network_Contains(p,c) and p['index'] < c['index']):
-          if p['ip'] == c['ip']: 
-            if c['mask'] > p['mask']: c['parent'] = p['id']
-          else: c['parent'] = p['id']
-  return entries
+    for p in entries:
+        for c in entries:
+            if c['index'] is not p['index']:
+                if (IPv4Network_Contains(p, c) and p['index'] < c['index']):
+                    if p['ip'] == c['ip']:
+                        if c['mask'] > p['mask']:
+                            c['parent'] = p['id']
+                    else:
+                        c['parent'] = p['id']
+    return entries
+
 
 def add_utilisation(entries):
-  for i in entries:
-    count = pow(2, (32 - i['mask']))
-    result = IPv4Address.query.filter(((IPv4Address.ip >= i['ip']) & (IPv4Address.ip <= i['ip']+count)) & IPv4Address.ip_domain_id == i['ip_domain_id']).all()
-    
-    app.logger.error(len(result))
-    
-    
-    
-    i['utilisation_string'] = str(len(result)) + " / " +  str(count)
+    for i in entries:
+        count = pow(2, (32 - i['mask']))
+        result = IPv4Address.query.filter(((IPv4Address.ip >= i['ip']) & (IPv4Address.ip <= i['ip'] + count)) & IPv4Address.ip_domain_id == i['ip_domain_id']).all()
 
-    i['utilisation_pct'] = (float(len(result)) / float(count)) * 100
-    app.logger.error(i)
-  return entries
-    
+        app.logger.error(len(result))
+
+        i['utilisation_string'] = str(len(result)) + " / " + str(count)
+        i['utilisation_pct'] = (float(len(result)) / float(count)) * 100
+        app.logger.error(i)
+    return entries
+
+
 def IPv4Network_Contains(parent_entry, child_entry):
-  start = parent_entry['ip']
-  end = parent_entry['ip'] - 1 + pow(2,  (32 - parent_entry['mask']))
-  if (child_entry['ip'] >= start and child_entry['ip'] <= end):
-    return True
-  else:
-    return False
+    start = parent_entry['ip']
+    end = parent_entry['ip'] - 1 + pow(2, (32 - parent_entry['mask']))
+    if (child_entry['ip'] >= start and child_entry['ip'] <= end):
+        return True
+    else:
+        return False
 
-def db_query_IPv4Network(ip_domain_id , network_id):
-  entrylist = dict()
-  entry = IPv4Network.query.filter_by(ip_domain_id = ip_domain_id, id=network_id).first()
-  for value in vars(entry):
-    if value != '_sa_instance_state':
-      entrylist[value] = vars(entry)[value]
-  entrylist['ip_string'] = long2ip(entrylist['ip'])  + "/" + str(entrylist['mask'])
-  return entrylist
+
+def db_query_IPv4Network(ip_domain_id, network_id):
+    entrylist = dict()
+    entry = IPv4Network.query.filter_by(ip_domain_id=ip_domain_id, id=network_id).first()
+    for value in vars(entry):
+        if value != '_sa_instance_state':
+            entrylist[value] = vars(entry)[value]
+    entrylist['ip_string'] = long2ip(entrylist['ip']) + "/" + str(entrylist['mask'])
+    return entrylist
 
 
 def db_query_IPv4Address(ip_domain_id, ip, count):
-  """ Queries a list of ip addresses in a specified ip_domain, starting with ip (int value), up to the specified mask"""
-  entries = list()
-  for i in range (0, count):
-    entries.append({ "id": -1 ,"ip" : ip + i , "ip_string" : long2ip(ip + i) , "ip_domain_id": ip_domain_id, "fqdn":"", "description":"", "reserved": False })
-  
-  result = IPv4Address.query.filter(((IPv4Address.ip >= ip) & (IPv4Address.ip <= ip+count)) & IPv4Address.ip_domain_id == ip_domain_id).all()
-  
-  for i in range (0,len(entries)):
-    for entry in result:
-      if entries[i]['ip'] == entry.ip:
-        entries[i]['id'] = entry.id
-        entries[i]['fqdn'] = entry.fqdn
-        entries[i]['description'] = entry.description
-        entries[i]['reserved'] = entry.reserved         
-  return entries
+    """ Queries a list of ip addresses in a specified ip_domain, starting with ip (int value), up to the specified mask"""
+    entries = list()
+    for i in range(0, count):
+        entries.append({"id": -1, "ip": ip + i, "ip_string": long2ip(ip + i), "ip_domain_id": ip_domain_id, "fqdn": "", "description": "", "reserved": False})
+
+    result = IPv4Address.query.filter(((IPv4Address.ip >= ip) & (IPv4Address.ip <= ip + count)) & IPv4Address.ip_domain_id == ip_domain_id).all()
+
+    for i in range(0, len(entries)):
+        for entry in result:
+            if entries[i]['ip'] == entry.ip:
+                entries[i]['id'] = entry.id
+                entries[i]['fqdn'] = entry.fqdn
+                entries[i]['description'] = entry.description
+                entries[i]['reserved'] = entry.reserved
+    return entries
+
 
 def db_change_IPv4Address(ip_domain_id, ip, data):
-  print ip_domain_id, ip, data
-  stmt = ""
-  for (entry) in data:
+    print ip_domain_id, ip, data
+    stmt = ""
+    for (entry) in data:
 
-    if entry == 'ip_domain_id':
-      stmt = update(IPv4Address).where((IPv4Address.ip==ip) & (IPv4Address.ip_domain_id == ip_domain_id) ).values(ip_domain_id=data[entry])
-    if entry == 'fqdn':
-      stmt = update(IPv4Address).where((IPv4Address.ip==ip) & (IPv4Address.ip_domain_id == ip_domain_id) ).values(fqdn=data[entry])
-    if entry == 'description':
-      stmt = update(IPv4Address).where((IPv4Address.ip==ip) & (IPv4Address.ip_domain_id == ip_domain_id) ).values(description=data[entry])
-    if entry == 'reserved':
-      stmt = update(IPv4Address).where((IPv4Address.ip==ip) & (IPv4Address.ip_domain_id == ip_domain_id) ).values(reserved=data[entry])
-    print stmt
-    result = db_session.execute(stmt)
-    print vars(result)
-  return stmt
-
-
- 
-
-
-
-
-
-
-
+        if entry == 'ip_domain_id':
+            stmt = update(IPv4Address).where((IPv4Address.ip == ip) & (IPv4Address.ip_domain_id == ip_domain_id)).values(ip_domain_id=data[entry])
+        if entry == 'fqdn':
+            stmt = update(IPv4Address).where((IPv4Address.ip == ip) & (IPv4Address.ip_domain_id == ip_domain_id)).values(fqdn=data[entry])
+        if entry == 'description':
+            stmt = update(IPv4Address).where((IPv4Address.ip == ip) & (IPv4Address.ip_domain_id == ip_domain_id)).values(description=data[entry])
+        if entry == 'reserved':
+            stmt = update(IPv4Address).where((IPv4Address.ip == ip) & (IPv4Address.ip_domain_id == ip_domain_id)).values(reserved=data[entry])
+        print stmt
+        result = db_session.execute(stmt)
+        print vars(result)
+    return stmt
